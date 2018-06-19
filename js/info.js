5a8e4b6479b18f084780aee44cb2128b56f81c90
unknown <marcos10junio28@gmail.com>
2018-06-19 00:33:50 -0500

    tec emergentes

---
+++ js/info.js
@@ -0,0 +1,59 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+
+
+//Leer de la base de datos
+firebase.database().ref("match")
+.on("child_added",function(s){
+	var user = s.val();
+	$('#view').append("<div class='container'>  <section class='cuadros'> <article> <img src="+user.foto+" alt='Avatar' class='image'>  <div class='overlay'>    <div class='text'>"+user.nombre+"</div>  </div>  </article> </section>  </div>")
+
+
+})
+
+
+
+function CerrarSesion(){
+            firebase.auth().signOut()
+            .then(function(){
+                console.log("Se ha cerrado Sesion");
+                provider.addScope('index.html');
+                Redirect('./index.html');
+            })
+            .catch(function(error){
+                console.log(error);
+            }) 
+}
+
+function Redirect (url) {
+        var ua        = navigator.userAgent.toLowerCase(),
+            isIE      = ua.indexOf('msie') !== -1,
+            version   = parseInt(ua.substr(4, 2), 10);
+
+        // Internet Explorer 8 and lower
+        if (isIE && version < 9) {
+            var link = document.createElement('a');
+            link.href = url;
+            document.body.appendChild(link);S
+            link.click();
+        }
+        // All other browsers
+        else { window.location.href = url; }
+    }
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
